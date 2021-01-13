fb6c30a35c80b992c3f3e1fef6ff4a9ca21aca85
Evgeniy Medvedev <eposs3@mail.ru>
2021-01-13 09:08:15 +0300

    upd exctention

--- Concentration1/ViewController.swift
+++ Concentration1/ViewController.swift
@@ -59,6 +59,13 @@ class ViewController: UIViewController {
 
 extension Int{
     var arc4randomExctention: Int {
-        return Int(arc4random_uniform(UInt32(self)))
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+        
     }
 }
